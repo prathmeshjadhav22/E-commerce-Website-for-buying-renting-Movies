59452e39539c559d4fd37202702bdceb960f23ff
Prathmesh Jadhav <yashj20559@gmail.com>
2021-09-18 15:19:49 +0530

    first commit

---
+++ npm/projects/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from.models import Project , Remarks ,contact
+# Register your models here.
+admin.site.register(Project)
+admin.site.register(Remarks)
+admin.site.register(contact)
